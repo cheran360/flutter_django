0f3668b36e7c34ecbc0549dbdfd91cb7a183e445
cheran <cherankumar.548@gmail.com>
2022-02-12 14:22:07 +0530

    half deadend

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +22,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
-late String stringResponse;
+String? stringResponse;
+late Map mapResponse;
+Map? dataResponse;
+List? listResponse;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -33,10 +37,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Future apicall() async {
     http.Response response;
-    response = await http.get(Uri.parse("https://reqres.in/api/users/2"));
+    response = await http.get(Uri.parse("https://reqres.in/api/users?page=2"));
     if (response.statusCode == 200) {
       setState(() {
-        stringResponse = response.body;
+        //stringResponse = response.body;
+        mapResponse = json.decode(response.body);
+        listResponse = mapResponse['data'];
       });
     }
   }
@@ -50,17 +56,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("API Demo"),
-      ),
-      body: Center(
-        child: Container(
-            height: 200,
-            width: 300,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20), color: Colors.blue),
-            child: Center(child: Text(stringResponse.toString()))),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("API Demo"),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Container(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.network(listResponse![index]['avatar']),
+                  ),
+                  Text(listResponse![index]['id'].toString()),
+                  Text(listResponse![index]['email'].toString()),
+                  Text(listResponse![index]['first_name'].toString()),
+                  Text(listResponse![index]['last_name'].toString()),
+                ],
+              ),
+            );
+          },
+          itemCount: listResponse == null ? 0 : listResponse!.length,
+        ));
   }
 }
